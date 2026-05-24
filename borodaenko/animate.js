dab1c6142cf1a637aa76e0b24070b0e76a0a2525
Sofia Borodaenko <soph.bor06@gmail.com>
2026-05-24 14:53:21 -0400

    added the hero reveal animation

--- borodaenko/animate.js
+++ borodaenko/animate.js
@@ -1,3 +1,5 @@
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 window.addEventListener("load", () => {
   const paths = document.querySelectorAll(".draw-path");
   const dots = document.querySelectorAll(".draw-dot");
@@ -31,8 +33,37 @@ window.addEventListener("load", () => {
 
   // animating the dots on hero section after the rectangle is complete
   dots.forEach((dot, index) => {
-    dot.animate(
-        [{fillOpacity: 1}], {duration: 1000, fill: "forwards", delay: paths.length * 280 + index * 300, easing: "ease"}
-    )
+    dot.animate([{ fillOpacity: 1 }], {
+      duration: 1000,
+      fill: "forwards",
+      delay: paths.length * 280 + index * 300,
+      easing: "ease",
+    });
   });
+
+  const lastPathEnd = 2500 + (paths.length - 1) * 280;
+
+  //const heroRevealTime = lastPathEnd + 50;
+
+  async function revealHeroContent() {
+    const heroNote = document.querySelector(".hero-note");
+    const heroTitle = document.querySelector(".hero-title");
+    const heroSubtitle = document.querySelector(".hero-subtitle");
+
+    heroNote.classList.remove("hiden");
+    heroNote.classList.add("reveal--opacity");
+    await delay(500);
+
+    heroTitle.classList.remove("hiden");
+    heroTitle.classList.add("reveal--opacity");
+    await delay(500);
+
+    heroSubtitle.classList.remove("hiden");
+    heroSubtitle.classList.add("reveal--opacity");
+    await delay(500);
+    document.body.style.overflow = "auto";
+  }
+
+  setTimeout(revealHeroContent, lastPathEnd);
+  //revealHeroContent();
 });
