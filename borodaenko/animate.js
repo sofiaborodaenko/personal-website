7cc94502489d78d3eda9734afdcb4af42314dbd7
Sofia Borodaenko <soph.bor06@gmail.com>
2026-05-25 16:11:50 -0400

    text appear on scroll about section

--- borodaenko/animate.js
+++ borodaenko/animate.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 window.addEventListener("load", () => {
@@ -50,15 +52,15 @@ window.addEventListener("load", () => {
     const heroTitle = document.querySelector(".hero-title");
     const heroSubtitle = document.querySelector(".hero-subtitle");
 
-    heroNote.classList.remove("hiden");
+    heroNote.classList.remove("hidden");
     heroNote.classList.add("reveal--opacity");
     await delay(500);
 
-    heroTitle.classList.remove("hiden");
+    heroTitle.classList.remove("hidden");
     heroTitle.classList.add("reveal--opacity");
     await delay(500);
 
-    heroSubtitle.classList.remove("hiden");
+    heroSubtitle.classList.remove("hidden");
     heroSubtitle.classList.add("reveal--opacity");
     await delay(500);
     document.body.style.overflow = "auto";
@@ -67,3 +69,58 @@ window.addEventListener("load", () => {
   setTimeout(revealHeroContent, lastPathEnd);
   //revealHeroContent();
 });
+
+function textOpacityScroll() {
+  const items = document.querySelectorAll(".about-me-text");
+
+  if (items.length) {
+    items.forEach((item) => {
+      const itemValue = item.querySelector(".text-section__value");
+      const itemMask = document.querySelector(".about-me-section__mask");
+
+        const itemSpeed = +itemValue.dataset.textSpeed || 500;
+        const itemOpacity = +itemValue.dataset.textOpacity || 0;
+
+        const words = itemValue.innerHTML.split(/(\s+|<br\s*\/?>)/);
+
+        itemValue.innerHTML = words
+          .map((word) => {
+            console.log(word);
+            if (word === "<br>") {
+              return "<br>";
+            } else if (word === 'class="scribble">2</span>3rd') {
+              return `<span style="transition: opacity ${itemSpeed}ms; opacity: ${itemOpacity};" class="scribble">2</span><span style="transition: opacity ${itemSpeed}ms; opacity: ${itemOpacity};">3rd</span>`;
+            } else {
+              return `<span style="transition: opacity ${itemSpeed}ms; opacity: ${itemOpacity};">${word}</span>`;
+            }
+          })
+          .join(" ");
+
+        window.addEventListener("scroll", () => {
+          const maskPosition =
+            itemMask.getBoundingClientRect().top - window.innerHeight;
+          const itemWay =
+            (Math.abs(maskPosition) /
+              (window.innerHeight + itemMask.offsetHeight)) *
+            100;
+          const itemWords = itemValue.querySelectorAll("span");
+          const currentWord =
+            maskPosition <= 0
+              ? Math.floor((itemWords.length / 100) * itemWay)
+              : -1;
+          addOpacity(itemWords, currentWord);
+        });
+
+        function addOpacity(words, current) {
+          words.forEach((word, index) => {
+            word.style.opacity = itemOpacity;
+            if (index <= current) {
+              word.style.opacity = 1;
+            }
+          });
+        }
+    });
+  }
+}
+
+textOpacityScroll();
